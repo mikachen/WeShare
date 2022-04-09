8d0aa93c15f665dea8f4b5e06c9c27520a5c3389
ZoeMac <twinkle74426@gmail.com>
2022-04-09 21:06:37 +0800

    swipealbe textview

--- app/src/main/java/com/zoe/weshare/home/HomeFragment.kt
+++ app/src/main/java/com/zoe/weshare/home/HomeFragment.kt
@@ -1,16 +1,26 @@
 package com.zoe.weshare.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DiffUtil
+import com.yuyakaido.android.cardstackview.*
 import com.zoe.weshare.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), CardStackListener {
+
+    private lateinit var cardStackView: CardStackView
+    private lateinit var binding: FragmentHomeBinding
+    private val manager by lazy { CardStackLayoutManager(requireContext(), this) }
+    private val adapter by lazy { CardStackAdapter(createSpot()) }
 
 
     override fun onCreateView(
@@ -18,9 +28,175 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
 
-        val binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+
+        setupCardStackView()
+
+
+
+        return binding.root
     }
 
+    private fun setupCardStackView() {
+        cardStackView = binding.cardStackView
+        manager.setStackFrom(StackFrom.None)
+        manager.setVisibleCount(3)
+        manager.setTranslationInterval(8.0f)
+        manager.setScaleInterval(0.95f)
+        manager.setSwipeThreshold(0.3f)
+        manager.setMaxDegree(20.0f)
+        manager.setDirections(Direction.HORIZONTAL)
+        manager.setCanScrollHorizontal(true)
+        manager.setCanScrollVertical(true)
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+        manager.setOverlayInterpolator(LinearInterpolator())
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = adapter
+        cardStackView.itemAnimator.apply {
+            if (this is DefaultItemAnimator) {
+                supportsChangeAnimations = false
+            }
+        }
+    }
+
+    override fun onCardDragging(direction: Direction, ratio: Float) {
+        Log.d("CardStackView", "onCardDragging: d = ${direction.name}, r = $ratio")
+    }
+
+    override fun onCardSwiped(direction: Direction) {
+        Log.d("CardStackView", "onCardSwiped: p = ${manager.topPosition}, d = $direction")
+        if (manager.topPosition == adapter.itemCount - 5) {
+            paginate()
+        }
+    }
+
+    override fun onCardRewound() {
+        Log.d("CardStackView", "onCardRewound: ${manager.topPosition}")
+    }
+
+    override fun onCardCanceled() {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
+    }
+
+    override fun onCardAppeared(view: View, position: Int) {
+//        val textView = view.findViewById<TextView>(R.id.item_name)
+//        Log.d("CardStackView", "onCardAppeared: ($position) ${textView.text}")
+    }
+
+    override fun onCardDisappeared(view: View, position: Int) {
+//        val textView = view.findViewById<TextView>(R.id.item_name)
+//        Log.d("CardStackView", "onCardDisappeared: ($position) ${textView.text}")
+    }
+
+    private fun paginate() {
+        val old = adapter.getSpots()
+        val new = old.plus(createSpot())
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun reload() {
+        val old = adapter.getSpots()
+        val new = createSpot()
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun addFirst(size: Int) {
+        val old = adapter.getSpots()
+        val new = mutableListOf<String>().apply {
+            addAll(old)
+            for (i in 0 until size) {
+                addAll(manager.topPosition, createSpot())
+            }
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun addLast(size: Int) {
+        val old = adapter.getSpots()
+        val new = mutableListOf<String>().apply {
+            addAll(old)
+//            addAll(List(size) { createSpot() })
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun removeFirst(size: Int) {
+        if (adapter.getSpots().isEmpty()) {
+            return
+        }
+
+        val old = adapter.getSpots()
+        val new = mutableListOf<String>().apply {
+            addAll(old)
+            for (i in 0 until size) {
+                removeAt(manager.topPosition)
+            }
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun removeLast(size: Int) {
+        if (adapter.getSpots().isEmpty()) {
+            return
+        }
+
+        val old = adapter.getSpots()
+        val new = mutableListOf<String>().apply {
+            addAll(old)
+            for (i in 0 until size) {
+                removeAt(this.size - 1)
+            }
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun replace() {
+        val old = adapter.getSpots()
+        val new = mutableListOf<String>().apply {
+            addAll(old)
+            removeAt(manager.topPosition)
+            addAll(manager.topPosition, createSpot())
+        }
+        adapter.setSpots(new)
+        adapter.notifyItemChanged(manager.topPosition)
+    }
+
+    private fun swap() {
+        val old = adapter.getSpots()
+        val new = mutableListOf<String>().apply {
+            addAll(old)
+            val first = removeAt(manager.topPosition)
+            val last = removeAt(this.size - 1)
+            add(manager.topPosition, last)
+            add(first)
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun createSpot(): List<String> {
+        val mocklist = listOf("apple", "banana", "grape", "orange", "grape", "esonchen")
+        return mocklist
+    }
 }
